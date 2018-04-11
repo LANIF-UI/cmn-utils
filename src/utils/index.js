3ae50f68ffc138e12eaf8a961ef19b6dc85757f1
weiq <6682220aabc@163.com>
2018-04-11 16:40:47 +0800

    增加防抖和节流函数

--- src/utils/index.js
+++ src/utils/index.js
@@ -30,7 +30,7 @@ export function getQueryObject() {
   return (function(a) {
     if (a == '') return {};
     const b = {};
-    for (const i = 0; i < a.length; ++i) {
+    for (let i = 0; i < a.length; ++i) {
       const p = a[i].split('=');
       if (p.length != 2) continue;
       b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, ' '));
